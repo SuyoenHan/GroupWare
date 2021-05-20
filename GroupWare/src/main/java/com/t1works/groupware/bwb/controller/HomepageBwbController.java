3584969e260429d444ec033dcc86656ddd65e6bb
WONBIN <bwb930305@naver.com>
2021-05-20 11:03:51 +0900

    백원빈_web.xml 및 패키지명 수정

--- GroupWare/src/main/java/com/t1works/groupware/bwb/controller/HomepageBwbController.java
+++ GroupWare/src/main/java/com/t1works/groupware/bwb/controller/HomepageBwbController.java
@@ -1,10 +1,10 @@
-package com.t1works.groupware.controller.bwb;
+package com.t1works.groupware.bwb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class TestControllerBwB {
+public class HomepageBwbController {
 	
 	@RequestMapping(value="/t1/home.tw")
 	public String test_test1() {
