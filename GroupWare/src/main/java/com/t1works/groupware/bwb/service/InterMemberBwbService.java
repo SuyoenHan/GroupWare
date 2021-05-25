ee0e2106d9fc411bea2c22c48c7e6676e3d6bac8
WONBIN <bwb930305@naver.com>
2021-05-25 23:23:06 +0900

    백원빈_인사관리 완료 _commit

--- GroupWare/src/main/java/com/t1works/groupware/bwb/service/InterMemberBwbService.java
+++ GroupWare/src/main/java/com/t1works/groupware/bwb/service/InterMemberBwbService.java
@@ -1,5 +1,24 @@
 package com.t1works.groupware.bwb.service;
 
+import java.util.List;
+import java.util.Map;
+
+import com.t1works.groupware.bwb.model.MemberBwbVO;
+
 public interface InterMemberBwbService {
+	
+	// 모든직위 가져오기
+	List<MemberBwbVO> selectPositionList();
+	
+	// 소속에 따른 직무설명 가져오기
+	String selectDuty(String dname);
+	
+	// pname과 dname을 통해 pcode,dcode 가져오기.
+	Map<String,String> selectPDcode(Map<String, String> paraMap);
+	
+	// 회원정보 업데이트하기
+	int updateOneInfo(MemberBwbVO mvo);
+	
+	
 
 }
