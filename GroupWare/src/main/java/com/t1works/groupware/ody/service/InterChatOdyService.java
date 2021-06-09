4eae5a940260594ad6d1c50e8a9721bb8510c1bc
OhDayoon <dayoon1013@gmail.com>
2021-06-09 15:57:27 +0900

    웹채팅 commit

--- GroupWare/src/main/java/com/t1works/groupware/ody/service/InterChatOdyService.java
+++ GroupWare/src/main/java/com/t1works/groupware/ody/service/InterChatOdyService.java
@@ -1,6 +1,7 @@
 package com.t1works.groupware.ody.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.t1works.groupware.ody.model.MemberOdyVO;
 
@@ -13,5 +14,8 @@ public interface InterChatOdyService {
 
 	// 사원정보 조회
 	List<MemberOdyVO> getEmployeeList(String employeeid);
+
+	// 이름 가져오기
+	Map<String,String> findChatName(String empId);
 
 }
--- GroupWare/src/main/java/com/t1works/groupware/ody/websockethandler/WebsocketEchoOdyHandler.java
+++ GroupWare/src/main/java/com/t1works/groupware/ody/websockethandler/WebsocketEchoOdyHandler.java
@@ -1,6 +1,8 @@
 package com.t1works.groupware.ody.websockethandler;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -131,6 +133,12 @@ public class WebsocketEchoOdyHandler extends TextWebSocketHandler {
         */
         String hostAddress = "";
  
+        // 시간
+        Calendar currentDate = Calendar.getInstance(); // 현재날짜와 시간을 얻어온다.
+	    SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+	    String currentTime = dateFormat.format(currentDate.getTime());
+	      
+        
         for (WebSocketSession webSocketSession : connectedUsers) {
             if (messageVO.getType().equals("all")) { 
                // 채팅할 대상이 "전체" 일 경우
@@ -140,7 +148,7 @@ public class WebsocketEchoOdyHandler extends TextWebSocketHandler {
                    // webSocketSession 은 웹소켓서버에 연결된 모든 클라이언트중 하나임.
                    // wsession.getId() 와  webSocketSession.getId() 는 자동증가되는 고유한 숫자로 나옴 
                     webSocketSession.sendMessage(
-                            new TextMessage("<span class='ip' style='display: none;'>"+wsession.getRemoteAddress().getAddress().getHostAddress()+"</span><span class='loginuserid' style='display: none;'>"+loginuser.getEmployeeid()+"</span>&nbsp;<p style='font-size:11pt; ' class='loginuserName'>" +loginuser.getName()+ "</p>" + "<span style='background-color: #fff; padding: 3px 2px;'>"+messageVO.getMessage()+"</span>"));  
+                            new TextMessage("<span class='ip' style='display: none;'>"+wsession.getRemoteAddress().getAddress().getHostAddress()+"</span><span class='loginuserid' style='display: none;'>"+loginuser.getEmployeeid()+"</span>&nbsp;<p style='font-size:11pt; ' class='loginuserName'>" +loginuser.getName()+ "</p>" + "<span>"+messageVO.getMessage()+"</span>&nbsp;<span style='font-size: 10pt;'>"+currentTime+"</span>"));  
                 }
             } 
             else { // 채팅할 대상이 "전체"가 아닌 특정대상(지금은 귓속말대상 IP address 임) 일 경우 
@@ -152,7 +160,7 @@ public class WebsocketEchoOdyHandler extends TextWebSocketHandler {
                     webSocketSession.sendMessage(
                             new TextMessage(
 
-                                    "<span class='ip' style='display: none;'>"+ wsession.getRemoteAddress().getAddress().getHostAddress() +"</span><span class='loginuserid' style='display: none;'>"+loginuser.getEmployeeid()+"</span>&nbsp;<p style='font-size:11pt;' class='loginuserName'>" +loginuser.getName()+ "</p>"  + "<span style='background-color: #fff; padding: 3px 2px;'>"+messageVO.getMessage()+"</span>"
+                                    "<span class='ip' style='display: none;'>"+ wsession.getRemoteAddress().getAddress().getHostAddress() +"</span><span class='loginuserid' style='display: none;'>"+loginuser.getEmployeeid()+"</span>&nbsp;<p style='font-size:11pt;' class='loginuserName'>" +loginuser.getName()+ "</p>"  + "<span>"+messageVO.getMessage()+"</span>&nbsp;<span  style='font-size: 10pt;'>"+currentTime+"</span>"
                                    ) 
                     );
                     break; // 지금의 특정대상(지금은 귓속말대상 IP address 임)은 1개이므로 
